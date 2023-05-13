a4c12ffda16e3af3bac0c7e5917176bf9bca79bd
amogh-opsverse <99157069+amogh-opsverse@users.noreply.github.com>
2023-05-13 15:08:34 -0700

    Update About.tsx

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -46,10 +46,7 @@ const About = () => {
                 "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 4px 6px rgba(0, 0, 0, 0.25)",
             }}
           >
-            In Greek, "ambrosia" is a noun that translates to "nectar". In the
-            Greek mythology, ambrosia is the food of the gods and is often
-            depicted as conferring longevity or immortality upon whoever
-            consumed it.
+          
           </p>
           <br />
           <p
